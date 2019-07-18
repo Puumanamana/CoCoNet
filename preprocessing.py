00e96a8a9c5787c965e0028f27f5818b7c5ed13a
Cedric Arisdakessian <carisdak@hawaii.edu>
2019-07-17 15:00:31 -1000

    experimental bam conversion

--- preprocessing.py
+++ preprocessing.py
@@ -23,111 +23,40 @@ def bam_to_depth(bam):
 def bam_to_h5(fasta,coverage_bam,output):
     ctg_info = { seq.id: len(seq.seq)
                  for seq in SeqIO.parse(fasta,"fasta") }
-    n_samples = len(coverage_bam)
-    
+
     coverage_h5_tmp = h5py.File("/tmp/coverage.h5",'w')
 
-    for i,bam in enumerate(coverage_bam):
+    for i,bam in enumerate(sorted(coverage_bam)):
         group = "sample_{}".format(i)
         print("Processing {}".format(group))
 
-        depth_buffer = np.zeros(1)
-        cur_ctg = ""
-        
         coverage_h5_tmp.create_group(group)
 
-        depth_file = bam_to_depth(bam)
+        depth_file  = bam_to_depth(bam)
+        cur_ctg = ""
+
         with open(depth_file) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter='\t')
-            
-            for ctg,pos,d in csv_reader:
+
+            for i,(ctg,pos,d) in enumerate(csv_reader):
                 if ctg != cur_ctg:
-                    coverage_h5_tmp.create_dataset("{}/{}".format(group,ctg),depth_buffer)
+                    if i > 0:
+                        coverage_h5_tmp.create_dataset("{}/{}".format(group,cur_ctg),data=depth_buffer)
+                    cur_ctg = ctg
                     depth_buffer = np.zeros(ctg_info[ctg],dtype=np.uint32)
 
-                depth_buffer[pos-1] = d
+                depth_buffer[int(pos)-1] = int(d)
 
-            coverage_h5_tmp.create_dataset("{}/{}".format(group,ctg),depth_buffer)
+            coverage_h5_tmp.create_dataset("{}/{}".format(group,ctg),data=depth_buffer)
 
-        # Save eerything in a [N_samples,Genome_size] matrix
-        coverage_h5 = h5py.File(output,'w')
-
-        for ctg in ctg_info:
-            try:
-                coverage_samples = [ coverage_h5_tmp.get("sample_{}/{}".format(i,ctg))
-                                     for i in range(len(coverage_bam)) ]
-            except:
-                import ipdb;ipdb.set_trace()
-            coverage = np.hstack([cov for cov in coverage_samples])
-            coverage_h5.create_dataset(ctg,coverage_h5)
-                        
-                
-                
-        # depth = pysam.depth(bam,'-d','200000')
-        # depth = [ x.split("\t") for x in depth.split('\n')
-        #           if len(x) > 0 ]
-        # depth = [ list(g) for k,g in groupby(depth,key=lambda x:x[0]) ]
-        
-        # for j,d in enumerate(depth):
-        #     ctg = d[0][0]
-        #     tmp_file = "/tmp/{}.npy".format(ctg)
-
-        #     if not os.path.exists(tmp_file):
-        #         coverage = np.zeros([n_samples,ctg_info[ctg]]).astype(int)
-        #     else:
-        #         coverage = np.load(tmp_file)
-            
-        #     data = np.array(d)
-        #     pos = data[:,1].astype(int)
-        #     pos_cov = data[:,2].astype(int)
-
-        #     coverage[i,pos-1] = pos_cov
-
-        #     np.save(tmp_file,coverage)
-            
-        #     print("{:,}/{:,}".format(j,len(depth)),end='\r')
-
-    coverage_h5 = h5py.File(output)
+    # Save everything in a [N_samples,Genome_size] matrix
+    coverage_h5 = h5py.File(output,'w')
 
     for ctg in ctg_info:
-        tmp_file = "/tmp/{}.npy".format(ctg)
+        coverage_samples = [ coverage_h5_tmp.get("sample_{}/{}".format(i,ctg)[:])
+                             for i in range(len(coverage_bam)) ]
+        coverage_samples = [ x if x is not None else np.zeros(ctg_info[ctg],dtype=np.uint32)
+                             for x in coverage_samples ]
 
-        if os.path.exists(tmp_file):
-            coverage = np.load(tmp_file)
-            os.remove(tmp_file)
-        else:
-            coverage = np.zeros([n_samples,ctg_info[ctg]]).astype(int)
-            
+        coverage = np.vstack([cov for cov in coverage_samples])
         coverage_h5.create_dataset(ctg,data=coverage)
-    coverage_h5.close()
-    
-# def bam_to_h5(fasta,coverage_bam,output):
-#     ctg_info = [ (seq.id, len(seq.seq))
-#                  for seq in SeqIO.parse(fasta,"fasta") ]
-#     n_samples = len(coverage_bam)
-
-#     coverage_h5 = h5py.File(output)
-
-#     for ctg, length in progressbar(ctg_info):
-#         coverage = np.zeros([n_samples,length])
-        
-#         for i,bam in enumerate(coverage_bam):
-#             depth = get_depth(bam,ctg,length)
-
-#             if depth.size > 0:
-#                 positions = depth[:,1].astype(int)-1
-#                 cov = depth[:,2].astype(int)
-#                 coverage[i,positions] = cov
-
-#         coverage_h5.create_dataset(ctg,data=coverage)
-
-# def get_depth(bam_file,ctg,length):
-    
-#     target_region = "{}:1-{}".format(ctg,length)
-#     depth = pysam.depth(bam_file, '-r', target_region, '-d', '200000')
-
-#     coverage = np.array([ x.split('\t') for x in depth.split('\n')
-#                           if len(x) > 0 ])
-
-#     return coverage
-
