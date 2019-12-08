f2817e46cb3be5fb39ed51b4a70a8376549d2a4e
Cedric Arisdakessian <carisdak@hawaii.edu>
2019-12-08 09:17:48 -1000

    cli test

--- tests/test_coconet.py
+++ tests/test_coconet.py
@@ -2,17 +2,37 @@
 Tests for main algorithm
 '''
 
-from coconet.coconet import main, run
+from pathlib import Path
+
+from click.testing import CliRunner
+
+from coconet.coconet import main
+
+LOCAL_DIR = Path(__file__).parent
 
 def test_main():
     '''
     Test greeter
     '''
 
-    # main()
+    runner = CliRunner()
+    result = runner.invoke(main)
+
+    assert result.exit_code == 0
 
 def test_all():
     '''
+    test parser and overall app
     '''
 
-    # run()
+    pass
+    # runner = CliRunner()
+    # fasta = '{}/sim_data/'.format(LOCAL_DIR)
+    # bam = ['{}/sim_data/sample_{}.bam'.format(LOCAL_DIR, i) for i in range(1, 3)]
+    # main_runner = runner.invoke(main, ['run']).runner
+
+    # main_runner.invoke('run')
+    # main_runner.env = {'fasta': fasta, 'coverage': bam}
+    
+    # assert result.exit_code == 0
+
