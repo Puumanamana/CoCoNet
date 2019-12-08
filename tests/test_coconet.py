8fbf3a00a98b37f76dd9ce2301c0b40a8e5edd8b
Cedric Arisdakessian <carisdak@hawaii.edu>
2019-12-08 02:25:27 -1000

    added samtools dependency

---
+++ tests/test_coconet.py
@@ -0,0 +1,18 @@
+'''
+Tests for main algorithm
+'''
+
+from coconet.coconet import main, run
+
+def test_main():
+    '''
+    Test greeter
+    '''
+
+    # main()
+
+def test_all():
+    '''
+    '''
+
+    # run()
