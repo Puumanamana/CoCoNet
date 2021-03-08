a83b5d1b3cae9f9e18d21b6cc867615305869d1e
Cedric A <carisdak@hawaii.edu>
2021-03-07 22:50:40 -1000

    v0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Setup config for coconet-binning package
 from setuptools import setup, find_packages
 
 setup(name='coconet-binning',
-      version='0.8.0',
+      version='0.9.0',
       description='A contig binning tool from viral metagenomes',
       long_description_content_type='text/x-rst',
       long_description=open('README.rst').read(),
