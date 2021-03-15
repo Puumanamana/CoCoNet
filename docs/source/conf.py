5453d45c12fe4a2720e72e6ef1d3c507ff8c2a60
Cedric Arisdakessian <carisdak@hawaii.edu>
2021-03-15 19:23:44 +0000

    fixed small doc issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ project = 'CoCoNet'
 author = 'Cedric Arisdakessian'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
